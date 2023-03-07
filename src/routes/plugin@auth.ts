b39e0de6f838ebb2ff832b0f4a56deeb0cae0843
ulic75 <ulic75@gmail.com>
2023-03-07 15:17:06 -0700

    mock settings

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -2,15 +2,13 @@ import { serverAuth$ } from '@builder.io/qwik-auth';
 import GitHub from '@auth/core/providers/github';
 import type { Provider } from '@auth/core/providers';
 
-export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(
-  ({ env }) => ({
-    secret: env.get('AUTH_SECRET'),
-    trustHost: true,
-    providers: [
-      GitHub({
-        clientId: env.get('GITHUB_ID')!,
-        clientSecret: env.get('GITHUB_SECRET')!,
-      }),
-    ] as Provider[],
-  })
-);
+export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } = serverAuth$(() => ({
+  secret: 'AUTH_SECRET',
+  trustHost: true,
+  providers: [
+    GitHub({
+      clientId: 'GITHUB_ID',
+      clientSecret: 'GITHUB_SECRET',
+    }),
+  ] as Provider[],
+}));
